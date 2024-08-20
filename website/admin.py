6cc7082ff3f89a93c1db85a4f08cad868d888fa8
Alex Zheng <75999032+smalex-z@users.noreply.github.com>
2024-08-20 16:17:27 -0400

    admin url fix

--- website/admin.py
+++ website/admin.py
@@ -2,8 +2,5 @@ from django.contrib import admin
 from .models import URL
 
 # Register your models here.
-@admin.register(URL)
-class URLAdmin(admin.ModelAdmin):
-    list_display = ('long_url', 'short_code', 'created_at')  # Display these fields in the admin list view
-    search_fields = ('long_url', 'short_code')  # Enable searching by these fields
-    list_filter = ('created_at',)  # Add filters for these fields
+admin.site.register(URL)
+
